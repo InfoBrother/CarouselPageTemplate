d2efcc3147b38c4215123b9434cfae23e83145ff
InfoBrother <Infobrother33@gmail.com>
2018-02-18 08:15:41 +0400

    Initial commit

---
+++ CarouselPageDemo/CarouselPageDemo/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+
+namespace CarouselPageDemo
+{
+	public partial class MainPage : CarouselPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            Children.Add(new FirstPage());
+            Children.Add(new SecondPage());
+            Children.Add(new ThirdPage());
+            Children.Add(new fourthPage());
+        }
+    }
+}
